088c8731822336b6cf3e3b9061b9af0f0c50e65b
tsubasa-utsumi <e.utsumin@gmail.com>
2022-07-14 13:45:31 +0900

    add viewer

---
+++ viewer/ComicFileManager/ComicFileManager/ComicFileManagerSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ComicFileManager
+{
+    public class ComicFileManagerSettings
+    {
+        public string LocalFolder { get; set; }
+        public string ServerFolder { get; set; }
+        public string RequestURL { get; set; }
+
+        public ComicFileManagerSettings()
+        {
+            // TODO: test
+            testSettings();
+        }
+
+        /// <summary>
+        /// テスト用初期化
+        /// </summary>
+        private void testSettings()
+        {
+            LocalFolder = @"I:\Comic\test";
+            ServerFolder = @"\\Server\ServerRAID\Comic\test";
+            RequestURL = "";
+        }
+    }
+}
---
+++ viewer/ComicFileManager/ComicFileManager/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComicFileManager
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ComicFileManagerSettings setting = new ComicFileManagerSettings();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Initialize();
+        }
+
+        #region 起動時処理
+        private void Initialize()
+        {
+            txtSearch.DataContext = this;
+        }
+        #endregion
+
+        #region 画面処理
+        private void ShowFolders()
+        {
+            lstFolders.DataContext = new ComicFolderViewModel();
+        }
+        #endregion
+
+        public void Search()
+        {
+            Console.Write("TEST");
+        }
+    }
+}

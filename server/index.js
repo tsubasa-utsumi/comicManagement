ee42faf9888dfc8855abcccf0497520c3d47b009
utsumi <utsumi@2022Desktop.localdomain>
2022-07-16 12:04:03 +0900

    rename asset folder

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = 12100
 
+const directory = "./symbolic"
+
 const sindex = require('serve-index')
 const options = {
 	icons: true,
 	filter: (filename, index, list, path) => {
 		return filename !== '@eaDir' 
 	},
-	template: __dirname + '/public/template.html'
+	template: __dirname + '/asset/template.html'
 }
-app.use('/',express.static('./comics'), sindex('./comics', options))
+
+// app.use(express.static(directory))
+app.use(sindex(directory, options))
 app.listen(PORT)
